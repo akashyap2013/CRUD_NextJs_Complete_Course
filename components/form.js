d39649194654321018eb5216368cf75ce280d2c3
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-06-23 18:32:42 +0530

    Form Created

---
+++ components/form.js
@@ -0,0 +1,40 @@
+export default function Form(){
+    return (
+        <form className="grid lg:grid-cols-2 w-4/6 gap-4">
+            <div className="input-type">
+                <input type="text" name="firstname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="FirstName" />
+            </div>
+            <div className="input-type">
+                <input type="text" name="firstname" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="LastName" />
+            </div>
+            <div className="input-type">
+                <input type="text" name="email" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Email" />
+            </div>
+            <div className="input-type">
+                <input type="text" name="salary" className="border w-full px-5 py-3 focus:outline-none rounded-md" placeholder="Salary" />
+            </div>
+            <div className="input-type">
+                <input type="date" name="date" className="border px-5 py-3 focus:outline-none rounded-md" placeholder="Salary" />
+            </div>
+
+
+            <div className="flex gap-10 items-center">
+                <div className="form-check">
+                    <input type="radio" value="Active" id="radioDefault1" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" />
+                    <label htmlFor="radioDefault1" className="inline-block tet-gray-800">
+                        Active
+                    </label>
+                </div>
+                <div className="form-check">
+                    <input type="radio" value="Inactive" id="radioDefault2" name="status" className="form-check-input appearance-none rounded-full h-4 w-4 border border-gray-300  bg-white checked:bg-green-500 checked:border-green-500 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer" />
+                    <label htmlFor="radioDefault2" className="inline-block tet-gray-800">
+                        Inactive
+                    </label>
+                </div>
+            </div>
+
+            <button className="flex justify-center text-md w-2/6 bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-green-500 hover:text-green-500">Add</button>
+
+        </form>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { BiUserPlus } from "react-icons/bi";
 import Table from '../components/table';
+import Form from '../components/form';
 
 export default function Home() {
   return (
@@ -23,6 +24,9 @@ export default function Home() {
         </div>
 
         {/* collapsable form */}
+        <div className="container mx-auto py-5">
+          <Form></Form>
+        </div>
 
         {/* table */}
         <div className="container mx-auto">
