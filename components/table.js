46b0c9fe8b4dd49faba89149b2fda74c68a7bca2
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-01 18:18:35 +0530

    React Query Added

--- components/table.js
+++ components/table.js
@@ -1,10 +1,13 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import data from '../database/data.json'
 import { getUser } from "../lib/helper";
+import { useQuery } from 'react-query';
 
 export default function Table(){
 
-    getUser().then( res => console.log(res))
+    const { isLoading, isError, data, error } = useQuery('users', getUser)
+
+    if(isLoading) return <div>Employee is Loading...</div>;
+    if(isError) return <div>Got Error {error}</div>
 
     return (
         <table className="min-w-full table-auto">
@@ -56,7 +59,7 @@ function Tr({id, name, avatar, email, salary, date, status }){
             <span>{date || "Unknown"}</span>
         </td>
         <td className="px-16 py-2">
-            <button className="cursor"><span className="bg-green-500 text-white px-5 py-1 rounded-full">{status || "Unknown"}</span></button>
+            <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
         </td>
         <td className="px-16 py-2 flex justify-around gap-5">
             <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+
+// create a client
+const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+  </QueryClientProvider>
+  )
 }
 
 export default MyApp
