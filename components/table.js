b18f0b70381e4e5387003cfa4397a1ee063dc43c
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-01 18:33:59 +0530

    Helper Functions Added

--- components/table.js
+++ components/table.js
@@ -1,10 +1,10 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
-import { getUser } from "../lib/helper";
+import { getUsers } from "../lib/helper";
 import { useQuery } from 'react-query';
 
 export default function Table(){
 
-    const { isLoading, isError, data, error } = useQuery('users', getUser)
+    const { isLoading, isError, data, error } = useQuery('users', getUsers)
 
     if(isLoading) return <div>Employee is Loading...</div>;
     if(isError) return <div>Got Error {error}</div>
