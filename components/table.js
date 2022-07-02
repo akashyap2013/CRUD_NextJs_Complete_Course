df5e929b829821a64bae088098afe03fb171e338
akshay kashyap <akshaykashyap.2013@gmail.comgit config -- global user.name akashyap2013>
2022-07-02 18:20:21 +0530

    Dispatch Action Added

--- components/table.js
+++ components/table.js
@@ -1,13 +1,11 @@
 import { BiEdit, BiTrashAlt } from "react-icons/bi";
 import { getUsers } from "../lib/helper";
 import { useQuery } from 'react-query';
-import { useSelector} from 'react-redux'
-
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleChangeAction } from '../redux/reducer'
 
 export default function Table(){
-    
-    const visible = useSelector((state) => state)
-    console.log(visible)
+
     const { isLoading, isError, data, error } = useQuery('users', getUsers)
 
     if(isLoading) return <div>Employee is Loading...</div>;
@@ -48,6 +46,14 @@ export default function Table(){
 
 function Tr({id, name, avatar, email, salary, date, status }){
 
+    const visible = useSelector((state) => state.app.client.toggleForm)
+    const dispatch = useDispatch()
+
+    const onUpdate = () => {
+        dispatch(toggleChangeAction())
+        console.log(visible)
+    }
+
     return (
         <tr className="bg-gray-50 text-center">
         <td className="px-16 py-2 flex flex-row items-center">
@@ -67,7 +73,7 @@ function Tr({id, name, avatar, email, salary, date, status }){
             <button className="cursor"><span className={`${status == "Active" ? 'bg-green-500' : 'bg-rose-500'} text-white px-5 py-1 rounded-full`}>{status || "Unknown"}</span></button>
         </td>
         <td className="px-16 py-2 flex justify-around gap-5">
-            <button className="cursor"><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
+            <button className="cursor" onClick={onUpdate} ><BiEdit size={25} color={"rgb(34,197,94)"}></BiEdit></button>
             <button className="cursor"><BiTrashAlt size={25} color={"rgb(244,63,94)"}></BiTrashAlt></button>
         </td>
     </tr>
